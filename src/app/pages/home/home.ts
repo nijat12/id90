617b66cde1cca893aa3d6d077b36a98768c3fffb
Nijat <nijat12@gmail.com>
2018-03-06 23:02:54 -0500

    Dragula hooks implemented

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { NguCarousel, NguCarouselStore, NguCarouselService } from '@ngu/carousel';
+import { DragulaService } from 'ng2-dragula';
 
 import { Card } from '../../models/card';
 import { cardService } from '../../services/card.service';
@@ -17,8 +18,43 @@ export class HomePage implements OnInit {
 
   constructor(public navCtrl: NavController,
     private carousel: NguCarouselService,
+    private dragulaService: DragulaService,
     private CardService: cardService) {
-
+      dragulaService.drag.subscribe((value) => {
+        console.log(`drag: ${value[0]}`);
+        this.onDrag(value.slice(1));
+      });
+      dragulaService.drop.subscribe((value) => {
+        console.log(`drop: ${value[0]}`);
+        this.onDrop(value.slice(1));
+      });
+      dragulaService.over.subscribe((value) => {
+        console.log(`over: ${value[0]}`);
+        this.onOver(value.slice(1));
+      });
+      dragulaService.out.subscribe((value) => {
+        console.log(`out: ${value[0]}`);
+        this.onOut(value.slice(1));
+      });
+  }
+  private onDrag(args) {
+    let [e, el] = args;
+    // do something
+  }
+  
+  private onDrop(args) {
+    let [e, el] = args;
+    // do something
+  }
+  
+  private onOver(args) {
+    let [e, el, container] = args;
+    // do something
+  }
+  
+  private onOut(args) {
+    let [e, el, container] = args;
+    // do something
   }
 
   initDataFn(key: NguCarouselStore) {
